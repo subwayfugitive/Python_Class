1677a68a9429685c0eafc192f84b45a63b688a6d
Thomas Powell Horan <horan_thomas@earthlink.net>
2022-03-29 19:37:29 -0700

    lab6

--- Lab 6/lab6.py
+++ Lab 6/lab6.py
@@ -1,52 +1,29 @@
 # Thomas Powell Horan Lab 6
 # list of all the months
 
-#default library that helps to check if a year is a leap year
-from calendar import isleap
-
-# list of months
-months_list  = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+months_list  = ["February", "March", "May", "June", "August", "April", "September", "January", "October", "November", "December","July"]
 
 def main():
     user_date = tuple((x.strip())for x in input("please enter the date in mm/dd/yyyy format that you want to convert, include '/' to seperate month day and year ").split('/'))
 
-    month = int(user_date[0])-1
+    month = int(user_date[0])
     day = int(user_date[1])
     year = int(user_date[2])
 
     mm = len(user_date[0])
     dd = len(user_date[1])
     yy = len (user_date[2])
-
-    correct_days = 0
-    # here we make sure that the month entered has the correct number of days
-    if month == 1 and day > 28 and isleap(year) == False:#here we check if the year is a leap year so that we can see if February has 28 or 29 days
-        print(f'Invalid date, {year} is not a leap year, so {months_list[month]} only has 28 days!')
-        correct_days +=1
-    if month == 3 and day > 30:
-        print(f'Invalid date, {months_list[month]} only has 30 days!')
-        correct_days +=1
-    if month == 5 and day > 30:
-        print(f'Invalid date, {months_list[month]} only has 30 days!')
-        correct_days +=1
-    if month == 8 and day > 30:
-        print(f'Invalid date, {months_list[month]} only has 30 days!')
-        correct_days +=1
-    if month == 10 and day > 30:
-        print(f'Invalid date, {months_list[month]} only has 30 days!')
-        correct_days +=1
-
-    if correct_days <= 0:
-        if month <= 11 and month >= 0 and mm == 2:
-            if day < 32 and day > 0 and dd == 2:
-                if year < 10000 and year > 0 and yy == 4:
-                    print(f"Valid Date: {months_list[month]} {day}, {year}")
-                else:
-                    print("ERROR invalid year, please enter a year less than 10,0000 and greater than 0 in YYYY format")
+ 
+    if month <= 11 and month >= 0 and mm == 2:
+        if day < 28 and day > 0 and dd == 2:
+            if year < 10000 and year > 0 and yy == 4:
+                print(f"Valid Date: {months_list[month]} {day}, {year}")
             else:
-                print("ERROR invaild day, please enter a day lower than 32 and more than 0 in DD format") 
+                print("##############")
         else:
-            print("ERROR invalid month, please enter a month 1-12 in MM format")
+            print("#################") 
+    else:
+        print("#########################")
 
 if __name__ == '__main__':
     main()
