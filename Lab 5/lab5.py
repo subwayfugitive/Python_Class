be7307330b9bcaec98619866d48bfff15b78d480
Thomas Powell Horan <horan_thomas@earthlink.net>
2022-03-29 19:10:34 -0700

    lab5

--- Lab 5/lab5.py
+++ Lab 5/lab5.py
@@ -12,24 +12,23 @@ for i in range(51):
     y_square.append((i*0.25)**2)
     y_cube.append((i*0.25)**3)
 
-print(y_line)
 
-# # title for plot
-# plt.title("LINE, CUBE, SQUARE")
+# title for plot
+plt.title("LINE, CUBE, SQUARE")
 
-# # plotting changing color and marker type (Method 1)
-# plt.plot(y_line,y_line, 'g:o')#green "O"
-# plt.plot(y_line, y_square,'b:*')#blue "*"
-# plt.plot(y_line, y_cube, 'r:H')#red hexagon
+# plotting changing color and marker type (Method 1)
+plt.plot(y_line,y_line, 'g:o')#green "O"
+plt.plot(y_line, y_square,'b:*')#blue "*"
+plt.plot(y_line, y_cube, 'r:H')#red hexagon
 
-# #legend(Method 2)
-# plt.legend(['line','square','cube'])
+#legend(Method 2)
+plt.legend(['line','square','cube'])
 
-# #grid on(Method 3)
-# plt.grid()
+#grid on(Method 3)
+plt.grid()
 
-# #label axis(Method 4)
-# plt.xlabel("X Axis")
-# plt.ylabel("Y Axis")
+#label axis(Method 4)
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
 
-# plt.show()
+plt.show()
