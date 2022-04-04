51d3e02813fa08ea4294e07793807de138ef68d2
Thomas Powell Horan <horan_thomas@earthlink.net>
2022-04-03 21:55:17 -0700

    lab 6

--- Lab 6/lab6_ex.py
+++ Lab 6/lab6_ex.py
@@ -5,9 +5,19 @@ month=int(user_date[0])-1
 day=int(user_date[1])
 year=int(user_date[2])
 
-if month < 11 and month >= 0:
-    if day < 32 and day >0:
-        if year < 10000:
+month_len = len(user_date[0])
+day_len = len(user_date[1])
+year_len =len(user_date[2])
+
+if month_len  > 2:
+    print("nope")
+
+
+
+
+if month <= 11 and month >= 0 and month_len <= 2:
+    if day < 32 and day > 0 and day_len <= 2:
+        if year < 10000 and year > 0 and year_len <= 4:
             print(f'Valid date: {months_list[month]} {day}, {year}')
         else:
             print("Invaild year")
